7ab1a0bfae9579fa11c31a03a16ea9d94739f8da
xiaren2 <68859141+xiaren2@users.noreply.github.com>
2025-10-09 11:25:06 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -71,8 +71,8 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "rtp2httpd" "stackia/rtp2httpd" "main" "pkg"
-UPDATE_PACKAGE "luci-app-rtp2httpd" "stackia/rtp2httpd" "main" "pkg"
+#UPDATE_PACKAGE "rtp2httpd" "stackia/rtp2httpd" "main" "pkg"
+#UPDATE_PACKAGE "luci-app-rtp2httpd" "stackia/rtp2httpd" "main" "pkg"
 
 #更新软件包版本
 UPDATE_VERSION() {
