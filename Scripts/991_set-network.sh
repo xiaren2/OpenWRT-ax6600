f01e28caacb9a9368f5d05a623d963e9009c5e5e
xiaren2 <68859141+xiaren2@users.noreply.github.com>
2025-10-11 06:18:32 +0800

    Update 991_set-network.sh

--- Scripts/991_set-network.sh
+++ Scripts/991_set-network.sh
@@ -3,31 +3,19 @@
 # 检查 network.globals.ula_prefix 是否存在且不为空
 ula_prefix=$(uci get network.globals.ula_prefix 2>/dev/null)
 
-if [ -n "$ula_prefix" ]; then
-	uci set dhcp.wan6=dhcp
-	uci set dhcp.wan6.interface='wan6'
-	uci set dhcp.wan6.ignore='1'
+if [ -z "$ula_prefix" ]; then
+    # 尝试生成随机的 ULA 前缀（fd00::/8）
+    random_ula_prefix=$(hexdump -n 5 -e '1/1 "fd%02x:"' /dev/urandom 2>/dev/null | sed 's/:$//')
+    
+    if [ -n "$random_ula_prefix" ]; then
+        # 如果成功生成随机 ULA，则使用它（格式如 fdXX:XXXX:XXXX::/48）
+        ula_prefix="${random_ula_prefix}::/48"
+    else
+        # 如果随机生成失败，则使用默认的 ULA 前缀
+        ula_prefix="fd32:f54e:36f7::/48"
+    fi
 
-	uci set dhcp.lan.force='1'
-	uci set dhcp.lan.ra='hybrid'
-	uci set dhcp.lan.ra_default='1'
-	uci set dhcp.lan.max_preferred_lifetime='1800'
-	uci set dhcp.lan.max_valid_lifetime='3600'
-
-	uci del dhcp.lan.dhcpv6
-	uci del dhcp.lan.ra_flags
-	uci del dhcp.lan.ra_slaac
-	uci add_list dhcp.lan.ra_flags='none'
-
-	uci commit dhcp
-
-	uci set network.wan6.reqaddress='try'
-	uci set network.wan6.reqprefix='auto'
-	uci set network.lan.ip6assign='64'
-	uci set network.lan.ip6ifaceid='eui64'
-	uci del network.globals.ula_prefix
-
-	uci commit network
+    # 设置 ULA 前缀
+    uci set network.globals.ula_prefix="$ula_prefix"
+    uci commit network
 fi
-
-exit 0
