25f2fa50db3cca8ef5bc9d2bc915aadede7f5f8f
xiaren2 <68859141+xiaren2@users.noreply.github.com>
2025-10-20 21:07:41 +0800

    Update 991_set-network.sh

--- Scripts/991_set-network.sh
+++ Scripts/991_set-network.sh
@@ -1,12 +1,8 @@
-ula=$(uci -q get network.globals.ula_prefix)
-if [ -z "$ula" ] || [ "$ula" = "auto" ]; then
-    r1=$(dd if=/dev/urandom bs=1 count=1 2>/dev/null | hexdump -e '1/1 "%02x"')
-    r2=$(dd if=/dev/urandom bs=2 count=1 2>/dev/null | hexdump -e '2/1 "%02x"')
-    r3=$(dd if=/dev/urandom bs=2 count=1 2>/dev/null | hexdump -e '2/1 "%02x"')
+[ "$(uci -q get network.globals.ula_prefix)" != "auto" ] && exit 0
 
-    uci -q batch <<-EOF >/dev/null
-        set network.globals.ula_prefix=fd$r1:$r2:$r3::/48
-        commit network
+uci -q batch <<-EOF >/dev/null
+	set network.globals.ula_prefix="$(hexdump -vn 5 -e '"fd" 1/1 "%02x:" 2/2 "%x:"' /dev/urandom):/48"
+	commit network
 EOF
-fi
+
 exit 0
