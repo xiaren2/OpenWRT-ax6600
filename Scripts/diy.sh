7c781263e0563680ad4b510de299e4fc144d2d30
xiaren2 <68859141+xiaren2@users.noreply.github.com>
2025-11-09 11:30:38 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -469,7 +469,10 @@ fix_openwrt_apk_versions() {
 
 fix_openwrt_apk_versions package
 
-# 🩹 修复 libwebsockets CMake 版本不兼容问题
-find feeds/packages/libs/libwebsockets/Makefile -exec sed -i '/CMAKE_OPTIONS/s/$/ -DCMAKE_POLICY_VERSION_MINIMUM=3.5/' {} \;
 
+# 🩹 修复 CMake <3.5 不兼容问题
+find feeds/packages/libs -maxdepth 2 -name "CMakeLists.txt" -type f \
+  -exec grep -q "cmake_minimum_required" {} \; \
+  -exec sed -i '1s/^.*$/cmake_minimum_required(VERSION 3.5)/' {} \; \
+  -print
 
