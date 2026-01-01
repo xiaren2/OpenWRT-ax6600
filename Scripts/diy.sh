2f19b8a5fda4294477462c97364a998cd4cf7f37
xiaren2 <68859141+xiaren2@users.noreply.github.com>
2026-01-01 09:29:20 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -199,10 +199,10 @@ provided_config_lines=(
     "CONFIG_PACKAGE_openssh-sftp-server=y"
    # "CONFIG_PACKAGE_luci-app-frpc=y" 
    #apk还是opkg
-    "CONFIG_OPKG_USE_CURL=y"
-    "CONFIG_PACKAGE_opkg=y"   
-   "CONFIG_USE_APK=n"
-    "CONFIG_PACKAGE_apk-openssl=n"
+  #  "CONFIG_OPKG_USE_CURL=y"
+  #  "CONFIG_PACKAGE_opkg=y"   
+   "CONFIG_USE_APK=y"
+    "CONFIG_PACKAGE_apk-openssl=y"
 	
    # "CONFIG_PACKAGE_luci-app-tailscale=y"
     #"CONFIG_PACKAGE_luci-app-msd_lite=y"
@@ -344,13 +344,13 @@ install -Dm755 "${GITHUB_WORKSPACE}/Scripts/99_set_argon_primary" "package/base-
 
 
 #修改软件源地址，当是opkg的时候
-install -Dm755 "${GITHUB_WORKSPACE}/Scripts/99-distfeeds.conf" "package/emortal/default-settings/files/99-distfeeds.conf"
-sed -i '/define Package\/default-settings\/install/a \
-\t$(INSTALL_DIR) $(1)/etc\n\t$(INSTALL_DATA) ./files/99-distfeeds.conf $(1)/etc/99-distfeeds.conf' \
-package/emortal/default-settings/Makefile
-sed -i "/exit 0/i\\
-[ -f \'/etc/99-distfeeds.conf\' ] && mv \'/etc/99-distfeeds.conf\' \'/etc/opkg/distfeeds.conf\'\n\
-sed -ri \'/check_signature/s@^[^#]@#&@\' /etc/opkg.conf\n" "package/emortal/default-settings/files/99-default-settings"
+#install -Dm755 "${GITHUB_WORKSPACE}/Scripts/99-distfeeds.conf" "package/emortal/default-settings/files/99-distfeeds.conf"
+#sed -i '/define Package\/default-settings\/install/a \
+#\t$(INSTALL_DIR) $(1)/etc\n\t$(INSTALL_DATA) ./files/99-distfeeds.conf $(1)/etc/99-distfeeds.conf' \
+#package/emortal/default-settings/Makefile
+#sed -i "/exit 0/i\\
+#[ -f \'/etc/99-distfeeds.conf\' ] && mv \'/etc/99-distfeeds.conf\' \'/etc/opkg/distfeeds.conf\'\n\
+#sed -ri \'/check_signature/s@^[^#]@#&@\' /etc/opkg.conf\n" "package/emortal/default-settings/files/99-default-settings"
 
 
 #解决 dropbear 配置的 bug
