d4b0c488c5ad53d226b4dcf8d1de0d7339cc4c5f
xiaren2 <68859141+xiaren2@users.noreply.github.com>
2026-06-03 06:42:04 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -53,24 +53,24 @@ if [ -d *"luci-app-mini-diskmanager"* ]; then
 fi
 
 #修改qca-nss-drv启动顺序
-NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
-if [ -f "$NSS_DRV" ]; then
-	echo " "
+#NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
+#if [ -f "$NSS_DRV" ]; then
+#	echo " "
 
-	sed -i 's/START=.*/START=85/g' $NSS_DRV
+#	sed -i 's/START=.*/START=85/g' $NSS_DRV
 
-	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
-fi
+#	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
+#fi
 
 #修改qca-nss-pbuf启动顺序
-NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
-if [ -f "$NSS_PBUF" ]; then
-	echo " "
+#NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
+#if [ -f "$NSS_PBUF" ]; then
+#	echo " "
 
-	sed -i 's/START=.*/START=86/g' $NSS_PBUF
+#	sed -i 's/START=.*/START=86/g' $NSS_PBUF
 
-	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
-fi
+#	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
+#fi
 
 #修复TailScale配置文件冲突
 #TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
